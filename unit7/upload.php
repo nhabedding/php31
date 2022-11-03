40d3df93a15f97bd99cacf1a3257fb5b6a6cec98
Nha <87482325+nhabedding@users.noreply.github.com>
2022-11-03 19:53:36 +0700

    Add files via upload

--- unit7/upload.php
+++ unit7/upload.php
@@ -5,9 +5,11 @@ session_start();
 function uploadFile($input_name,$target_dir,$allowtypes,$max_size,$override){
 	$upload_status=true;
 	$target_file = $target_dir .'/' .basename($_FILES[$input_name]['name']);
+	//đường dẫn mong muốn lưu vào
 	$errors=array();
 
 	$types = "";
+	//types lấy loại file
 
 	if (is_array($allowtypes)) {
 		foreach ($allowtypes as $key => $type) {
@@ -23,8 +25,8 @@ function uploadFile($input_name,$target_dir,$allowtypes,$max_size,$override){
 	}
 	$check=$_FILES[$input_name];
 	echo "<pre>";
-print_r($check);
-echo "</pre>";
+	print_r($check);
+	echo "</pre>";
 	if ($_FILES[$input_name]['error']!=0) {
 		$errors[] = "Dữ liệu upload bị lỗi";
 		$upload_status = false;
@@ -36,14 +38,18 @@ echo "</pre>";
 		$upload_status = false;
 
 	}
+	// allowtypes để kiểm tra xem đúng định dạng hay không như k cho up file ảnh
+	//so sánh có giống định mình muốn hay không biến chữ thường bằng chữ hoa strtolower
 	if ($_FILES[$input_name]['size'] > $max_size*1024*1024) {
 		$errors[] = "Không được upload ảnh lớn hơn $max_size (MB).";
 		$upload_status =false;
 	}
+	//kiểm tra để tránh đầy server
 	if (file_exists($target_file) && $override == false) {
 		$errors[] = "Tên file đã tồn tại trên server, không được ghi đè";
 		$upload_status = false;
 	}
+	//có được ghi đè lên file tiếp không
 
 	if ($upload_status) {
 		if(move_uploaded_file($_FILES[$input_name]['tmp_name'], $target_file)){
@@ -52,6 +58,7 @@ echo "</pre>";
 		else{
 			$errors[] = 'Có lỗi xảy ra khi upload file vui lòng kiểm tra lại';
 			return array(false,$errors);
+			//return để thông báo ra màn hình
 		}
 	}
 	else{
@@ -59,13 +66,11 @@ echo "</pre>";
 	}
 
 }
-
+//Hàm move_uploaded_file() dùng để di chuyển tập tin được tải lên vào một nơi được chỉ định.
+//tmp_name là đường dẫn tạm
 $upload = uploadFile('avatar','images', array('jpg','jpeg','png','gif'),1,true);
 
 $_SESSION['upload_status']= $upload;
 header('Location: uploadFile.php');
-
-
-
 
  ?>
